24736951fe7c4f045382ca5ccc5e6419afa0666c
Ricardo Reales <ricardorealesabrams@gmail.com>
2022-02-02 14:02:07 -0300

    se agregaron estilos

--- practica_dom/app.js
+++ practica_dom/app.js
@@ -2,16 +2,16 @@ function recomendacionesImc(imc) {
     let mensaje = ""
     switch (true) {
         case (imc < 18.5):
-            mensaje = "usted  esta en desnutrición, recomendamos ponerse en control con un Nutricionista "
+            mensaje = "Usted  esta en desnutrición, recomendamos ponerse en control con un Nutricionista "
             break;
         case (imc >= 18.5 && imc <= 24.9):
-            mensaje = "usted  se encuentra dentro del rango de peso normal o saludable"
+            mensaje = "Usted  se encUentra dentro del rango de peso normal o saludable"
             break;
         case (imc >= 25 && imc <= 29.9):
-            mensaje = "usted se encuentra dentro del rango de sobrepeso, recomendamos ponerse en control con un Nutricionista y hacer ejercicicio "
+            mensaje = "Usted se encuentra dentro del rango de sobrepeso, recomendamos ponerse en control con un Nutricionista y hacer ejercicicio "
             break;
         default:
-            mensaje = "usted se encuentra dentro del rango de obesidad, recomendamos ponerse en control con un Nutricionista y hacer ejercicio"
+            mensaje = "Usted se encuentra dentro del rango de obesidad, recomendamos ponerse en control con un Nutricionista y hacer ejercicio"
             break;
     }
 
